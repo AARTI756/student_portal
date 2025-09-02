339f2ef5a3b68e4e4be1e847325df6177b0a1096
Aarti <asakpal756@gmail.com>
2025-09-02 12:48:38 +0530

    Initial commit - simple PHP portal

---
+++ about.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h2>About This Portal</h2>";
+echo "<p>Created for Experiment 4 demonstration.</p>";
+echo "<a href='index.php'>Back to Home</a>";
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Welcome to My Student Portal</h1>";
+echo "<p>This is a simple PHP app deployed on Azure.</p>";
+echo "<a href='about.php'>Go to About Page</a>";
+?>
